735256890316fb3b421a7f8365203946a3df874c
slgraf <slgraf@asu.edu>
2016-03-27 17:57:18 -0700

    Basic file transfer almost complete

--- client_test.py
+++ client_test.py
@@ -1,21 +1,29 @@
 import socket
+import os
 
 sock=socket.socket()
 
 host=socket.gethostname()
-port=5136
+port=5140
 
 print 'Client starting'
 
 sock.connect((host, port))
-sock.send('I am the client')
+# sock.send('I am the client')
 
 while True:
-	msg=raw_input()
-	if msg == 'q':
+	file=raw_input()
+	if os.path.exists(file):
+		with open(file, 'r') as f:
+			l=f.read(1024)
+			while l:
+				print 'Sending...'
+				sock.send(l)
+				l=f.read(1024)
+			print 'File sent!'
 		break
-	sock.send(msg)
+	else:
+		print 'error: {0} does not exist'.format(file)
 
 sock.send('Client disconnecting')
-
 sock.shutdown(socket.SHUT_WR)
--- server_test.py
+++ server_test.py
@@ -1,21 +1,35 @@
 import socket
+import os
+
+PATH='./server_files/file_{0}.txt'
 
 sock=socket.socket()
 
 host=socket.gethostname()
-port=5136
+port=5140
+
+file_num=0
 
 print 'Server start'
-
 sock.bind((host, port))
 sock.listen(5)
 while True:
 	conn, addr=sock.accept()
 	print 'Connection from ... {0}'.format(addr)
 
+	while os.path.exists(PATH.format(file_num)):
+		file_num+=1
+
 	try:
 		l=conn.recv(1024)
-		while l:
-			print 'Message from {0}: {1}'.format(addr, l)
-			l=conn.recv(1024)
+		with open(PATH.format(file_num), 'w') as f:
+			while l:
+				print 'Message from {0}'.format(addr)
+				f.write(l)
+				l=conn.recv(1024)
+			print 'File transfer success!'
 	except: pass
+
+sock.shutdown(socket.SHUT_WR)
+
+
